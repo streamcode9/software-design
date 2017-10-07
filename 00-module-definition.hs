40b841319be1287d2ea058125654efabe9670955
Alexander Slis <streamcode9@gmail.com>
2017-10-07 21:03:01 +0300

    Create 00-module-definition.hs

---
+++ 00-module-definition.hs
@@ -0,0 +1,14 @@
+--------------------------------------
+-- Header
+
+module SomeModuleName(someMethodDefinition, SomeTypeDefinition) where
+
+import SomeImportedModuleName0
+import SomeImportedModuleName1(someImportedMethodName)
+
+---------------------------------------
+-- Definitions (types, values, type classes, type class instances) 
+
+SomeTypeDefinition = Up | Down
+
+someMethodDefinition x = x * x 
