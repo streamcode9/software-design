ba56662f89e537d50c3f3989f3408f7bb9bbefaa
Alexander Slis <streamcode9@gmail.com>
2017-10-08 21:06:25 +0300

    Update 00-module-definition.hs

--- 00-module-definition.hs
+++ 00-module-definition.hs
@@ -21,3 +21,37 @@ data Day = Day Int
 data Date = Date Year Month Day
 
 
+--------------------------------------
+-- Control.Monad.Except
+
+throwError :: e -> m a
+catchError :: m a -> (e -> m a) -> m a
+
+catchError foo throwError = foo
+
+
+-- Desugar 'do notation'
+do { foo <- bar ; baz } = bar >>= \foo -> baz
+
+
+-- Eta-reduce
+\foo -> bar foo = bar
+
+
+--------------------------------------
+-- Control.Monad
+
+(>=>) :: Monad m => (a -> m b) -> (b -> m c) -> a -> m c
+(<=<) :: Monad m => (b -> m c) -> (a -> m b) -> a -> m c
+
+
+--------------------------------------
+-- Code style
+
+-- 1) Use (>=>), (<=<) and (.) to build one-directional stream
+
+
+
+
+
+
