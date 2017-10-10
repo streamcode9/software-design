caaef7829e03cf048aab96058c3f0e3ee550e001
Alexander Slis <streamcode9@gmail.com>
2017-10-10 13:42:31 +0300

    Update 00-module-definition.hs

--- 00-module-definition.hs
+++ 00-module-definition.hs
@@ -6,9 +6,11 @@ module SomeModuleName(someMethodDefinition, OutputTypeName) where
 import SomeImportedModuleName0
 import SomeImportedModuleName1(someImportedMethodName)
 
+
 ---------------------------------------
 -- Definitions (types, values, type classes, type class instances) 
 
+-- Alternatives enumeration = types sum
 data OutputTypeName = Up | Down
 
 data [a] = [] | a : [a]
@@ -18,12 +20,25 @@ someMethodDefinition x = Up
 
 data Day = Day Int
 
+-- Type consist of sub types -> constructor at first place and then sub types = types product
 data Date = Date Year Month Day
+
+-- Notice: if type has two sub types, we can give symbolic name to constructor instead of letter,
+-- but it must start from (:) symbol
+data MyType = Sub1 :+ Sub2
+
+-- Combined type with sum and product
+data Type = Name1  Sub11  Sub12 | Name2  Sub21  Sub22
 
 class Group a where
     e   :: a
     (+) :: a -> a -> a
     inv :: a -> a
+
+instance Group Bool where
+    e       = True
+    (+) a b = and a b
+    inv a   = not a
 
 twice :: Group a => a -> a
 twice a = a + a
@@ -38,6 +53,7 @@ class IsPerson a
 class IsPerson a => HasName a where
     name :: a -> String
 
+
 --------------------------------------
 -- Control.Monad.Except
 
@@ -47,10 +63,12 @@ catchError :: m a -> (e -> m a) -> m a
 catchError foo throwError = foo
 
 
+--------------------------------------
 -- Desugar 'do notation'
 do { foo <- bar ; baz } = bar >>= \foo -> baz
 
 
+--------------------------------------
 -- Eta-reduce
 \foo -> bar foo = bar
 
