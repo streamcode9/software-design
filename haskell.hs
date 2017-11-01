bb6e53a8d2cbf5e66aa717c6958613b6616a9925
Alexander Slis <streamcode9@gmail.com>
2017-11-01 17:32:24 +0200

    Update haskell.hs

--- haskell.hs
+++ haskell.hs
@@ -205,6 +205,12 @@ lift2 f a b = lift1 f a $$ b
 class Functor f where
     fmap :: (a -> b) -> f a -> f b
 
+class Functor f => Applicative f where
+    pure    :: a -> f a
+    (<*>)   :: f (a -> b) -> f a -> f b
 
+class Monad m where
+  return :: a -> m a
+  (>>=)  :: m a -> (a -> m b) -> m b
 
 
