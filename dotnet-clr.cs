f9ce45e6457a1ec5dc6a97b83124fc9c8207da95
Alexander Slis <streamcode9@gmail.com>
2017-11-21 21:26:34 +0200

    Update dotnet-clr.cs

--- dotnet-clr.cs
+++ dotnet-clr.cs
@@ -6,6 +6,11 @@ CLR functions: manage memory, load assemblies, security, handling exceptins, syn
 
 Src that supports CLR -> 
 check syntax & analyze (compiler) ->
-managed module = IL + metadata (pros: Data Execution Prevention, Address Space Layout Optimization)
+managed module (Portable Executable = PE32, PE32+) = IL + metadata (pros: Data Execution Prevention, Address Space Layout Optimization)
 
+Managed module:
+ 	PE32/+ header (type of file = CUI, GUI, DLL; assembly date)
+ 	CLR header (CLR ver, entry point, metadata & resources location, strict name)
+  	Metadata (current module data types and referenced ones) 
+ 	IL code
 */
