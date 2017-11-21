686a09e106779a90f15f5be9f17a75c3449d8d0b
Alexander Slis <streamcode9@gmail.com>
2017-11-21 21:13:15 +0200

    Create dotnet-clr.cs

---
+++ dotnet-clr.cs
@@ -0,0 +1,11 @@
+staic Main () {}
+
+/*
+
+CLR functions: manage memory, load assemblies, security, handling exceptins, synchronization, threads.
+
+Src that supports CLR -> 
+check syntax & analyze (compiler) ->
+managed module = IL + metadata (pros: Data Execution Prevention, Address Space Layout Optimization)
+
+*/
