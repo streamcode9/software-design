d9ae73c6f59671f85c2d7d5750a0633e3f139b5f
Alexander Slis <streamcode9@gmail.com>
2017-12-13 22:49:31 +0200

    Update dotnet-clr.cs

--- dotnet-clr.cs
+++ dotnet-clr.cs
@@ -11,6 +11,19 @@ managed module (Portable Executable = PE32, PE32+) = IL + metadata (pros: Data E
 Managed module:
  	PE32/+ header (type of file = CUI, GUI, DLL; assembly date)
  	CLR header (CLR ver, entry point, metadata & resources location, strict name)
-  	Metadata (current module data types and referenced ones) 
+  Metadata (current module data types and referenced ones) 
  	IL code
+
+Metadata:
+  - allows to avoid header files,
+  - helps VS (IntelliSense)
+  - CLR uses it for code verification,
+  - allows serialize object to send by network
+  - helps GC to track object life cycle
+  
+ Assembly:
+  - logical group of managed modules and resources
+  - minimal part of reusing, security, version control
+ 
+ Manifest - metadata tables that describe an assembly files, exported types, resource and data files
 */
