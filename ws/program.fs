7ca0722d07d0417f2eab4a30f850b9c767212195
streamcode9 <1745268+streamcode9@users.noreply.github.com>
2021-11-28 20:31:39 +0100

    Update program.fs

--- ws/program.fs
+++ ws/program.fs
@@ -3,6 +3,23 @@ open System.Collections.Specialized
 
 module Program =
 
+    type Msg =
+        | Bin of byte array
+        | Text of string
+        | Nope
+
+    type Sup =
+        | Connect of MailboxProcessor<Msg> * NetworkStream
+        | Disconnect of MailboxProcessor<Msg>
+        | Close of NetworkStream
+        | Tick
+
+    type Req =
+        { path    : string;
+          method  : string;
+          version : string;
+          headers : NameValueCollection }
+
     [<EntryPoint>]
     let main _ =
         let mutable port = 1900
