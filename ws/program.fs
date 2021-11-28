5899b4a66622bf737235e5add9d24855f42369b0
streamcode9 <streamcode9@gmail.com>
2021-11-28 13:20:12 +0100

    Update program.fs

--- ws/program.fs
+++ ws/program.fs
@@ -3,4 +3,12 @@
 module Program =
 
     [<EntryPoint>]
-    let main _ = -1
+    let main _ =
+        let mutable port = 1900
+        let mutable (cpu, io) = (0,0)
+        let mutable ret = 0
+
+        System.Threading.ThreadPool.GetMinThreads(&cpu, &io)
+        printfn "[smp] [cpu:%i] [io:%i]" cpu io
+
+        ret
