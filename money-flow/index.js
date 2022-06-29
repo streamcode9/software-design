2de9765589f04b8710f0a9f03619bd729c16cc50
streamcode9 <streamcode9@gmail.com>
2022-06-29 14:21:04 +0200

    moeny-flow: read from csv

--- money-flow/index.js
+++ money-flow/index.js
@@ -1,18 +1,39 @@
-const dict = {
- '2022-0':[
-  {inco:9,outc:0},
-  {inco:0,outc:8}
- ],
- '2022-1':[
-  {inco:5,outc:0},
-  {inco:0,outc:8}
- ]
-}
+const fs = require('fs');
 
-for (const [key, xs] of Object.entries(dict)) {
-  const ok =
-    xs.reduce((a,x)=>a+x.inco,0)
-    -xs.reduce((a,x)=>a+x.outc,0)
-  console.log(`${key}: ${ok}`);
-}
+fs.readFile('data.csv', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return
+  }
 
+  const rows = data.split('\r\n')
+    .filter(row => {
+        const cols = row.split(',')
+        return cols[0].length > 0
+    })
+    .filter(row => {
+        const cols = row.split(',')
+        const date = Date.parse(cols[0].slice(1, cols[0].length - 1) + ":00:00:00Z")
+        return !isNaN(date)
+    });
+
+  const dict = rows.reduce((acc, row) => {
+    const cols = row.split(',')
+    const date = new Date(Date.parse(cols[0].slice(1, cols[0].length - 1) + ":00:00:00Z"))
+    const typi = cols[4]
+    const desc = cols[5]
+    const outc  = cols[6]
+    const inco   = cols[7]
+
+    const key = date.getFullYear().toString() + '-' + date.getMonth().toString();
+    (acc[key] = acc[key] || []).push({ date, typi, desc, outc, inco })
+    return acc
+  }, {})
+
+  for (const [key, xs] of Object.entries(dict)) {
+    const ok =
+      xs.reduce((a,x)=>a+x.inco,0)
+      -xs.reduce((a,x)=>a+x.outc,0)
+    console.log(`${key}: ${ok}`);
+  }
+});
