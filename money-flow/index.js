475c9a6be432424f1a62b917fac93874457da3c6
streamcode9 <streamcode9@gmail.com>
2022-06-29 14:27:02 +0200

    Update index.js

--- money-flow/index.js
+++ money-flow/index.js
@@ -1,5 +1,17 @@
 const fs = require('fs');
 
+fs.readdir(__dirname, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file); 
+    });
+});
+
 fs.readFile('data.csv', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
