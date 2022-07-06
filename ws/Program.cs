e657857284b886115e3741fde8bdf132b535ff58
streamcode9 <streamcode9@gmail.com>
2022-07-06 08:38:36 +0200

    Update Program.cs

--- ws/Program.cs
+++ ws/Program.cs
@@ -1,2 +1,14 @@
 // https://www.youtube.com/channel/UCvIX9U9iNAQOfcubmDNKPOQ
-Console.Write(1);
+
+using System;
+
+namespace Ws // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
