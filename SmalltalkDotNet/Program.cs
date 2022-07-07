2fa765555f67ba6d24a3aba79c163f3e10e2bcc0
streamcode9 <streamcode9@gmail.com>
2022-07-07 20:01:24 +0200

    Create Program.cs

---
+++ SmalltalkDotNet/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Ws // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SmalltalkDotnet");
+        }
+    }
+}
